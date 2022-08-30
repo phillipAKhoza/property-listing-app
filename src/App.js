48ad7497f82f9581bda962232936dd6771fbf90b
=Gideon Ogongo <=ogjr80@gmail.com>
2022-08-30 16:13:24 +0200

    alter profile page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Explore from './pages/Explore';
 import Offers from './pages/Offers';
-// import Profile from './pages/Profile';
+import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
@@ -14,7 +14,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore/>}/>
           <Route path='/offers' element={<Offers/>}/>
-          <Route path='/profile' element={<SignIn/>}/>
+          <Route path='/profile' element={<Profile/>}/>
           <Route path='/sign-in' element={<SignIn/>}/>
           <Route path='/sign-up' element={<SignUp/>}/>
           <Route path='/forgot-password' element={<ForgotPassword/>}/>
