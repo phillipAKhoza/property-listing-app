9bc3f85d0a2beb42f033081bf86dfa34999693bb
phillip khoza <phillipantonk@gmail.com>
2022-08-30 19:28:25 +0200

    add sign in with google

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,9 +1,24 @@
+import {useLocation, useNavigate} from 'react-router-dom';
+import {getAuth, signInWithPopup, GoogleAuthProvider} from 'firebase/auth';
+import {doc, setDoc,getDoc} from 'firebase/firestore';
+import {db} from '../firebase.config';
+import {toast} from 'react-toastify';
+import googleIcon from '../assets/svg/googleIcon.svg';
+
 function OAuth() {
-    return ( 
-        <div>
-            OAuth
-        </div>
-     );
+    const location = useLocation();
+    const navigation = useNavigate();
+
+    const onGoogleclick = () =>{
+        
+    }
+
+    return <div className="socialLogin">
+        <p>Sign {location.pathname === '/sign-up' ? 'Up' : 'In'} with</p>
+        <button className="socialIconDiv" onClick={onGoogleclick}>
+            <img src={googleIcon} alt="google singin" />
+        </button>
+    </div>
 }
 
 export default OAuth;
