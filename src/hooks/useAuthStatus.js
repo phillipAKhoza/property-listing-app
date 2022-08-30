885525fbff2c91c6119bf24f831bc4f5dfe0dd9c
=Gideon Ogongo <=ogjr80@gmail.com>
2022-08-30 17:37:16 +0200

    useAuth custom hook

---
+++ src/hooks/useAuthStatus.js
@@ -0,0 +1,20 @@
+import { useState,useEffect} from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const useAuthStatus = () => {
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        const auth = getAuth()
+        onAuthStateChanged(auth, (user)=>{
+            if(user){
+                setLoggedIn(true);
+                setLoading(false);
+            }
+        })
+    })
+    return {loggedIn ,loading};
+}
+ 
+export default useAuthStatus;
