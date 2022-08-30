b18f0ba775d4d7a30559769b6d4dc1735455751a
=Gideon Ogongo <=ogjr80@gmail.com>
2022-08-30 17:37:00 +0200

    add a custome hook for authentication

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+const PrivateRoute = () => {
+
+    const loggedIn = false;
+
+    return loggedIn ? <Outlet/> : <Navigate to='/sign-in' />
+
+}
+ 
+export default PrivateRoute;
