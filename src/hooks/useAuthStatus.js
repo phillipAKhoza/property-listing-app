5bbd9a67408b02b5d2dbf83821a432436bd6f2bd
=Gideon Ogongo <=ogjr80@gmail.com>
2022-08-30 17:49:57 +0200

    memory leak fix using ismounted

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,19 +1,26 @@
-import { useState,useEffect} from "react";
+import { useState,useEffect, useRef} from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const useAuthStatus = () => {
     const [loggedIn, setLoggedIn] = useState(false);
     const [loading, setLoading] = useState(true);
+    const isMounted = useRef(true);
 
     useEffect(()=>{
-        const auth = getAuth()
-        onAuthStateChanged(auth, (user)=>{
-            if(user){
-                setLoggedIn(true);
+        if(isMounted){
+
+            const auth = getAuth()
+            onAuthStateChanged(auth, (user)=>{
+                if(user){
+                    setLoggedIn(true);
+                }
                 setLoading(false);
-            }
-        })
-    })
+            })
+        }
+        return ()=> {
+            isMounted.current = false
+        }
+    },[isMounted])
     return {loggedIn ,loading};
 }
  
