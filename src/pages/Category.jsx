45b522f4e2dcc7ec83f488643da526660884fef7
phillip khoza <phillipantonk@gmail.com>
2022-08-30 22:06:04 +0200

    implement listingitem

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -4,6 +4,7 @@ import { collection,  getDocs, query, where, orderBy, limit, startAfter} from "f
 import { db } from "../firebase.config";
 import { toast } from "react-toastify";
 import Spinner from '../components/Spinner';
+import ListingItem from '../components/ListingItm';
 
 function Category() {
 
@@ -44,7 +45,7 @@ function Category() {
             <ul className="categoryListings">
                 {
                     listings.map((listing)=>(
-                        <h3 key={listing.id}>{listing.data.name}</h3>
+                        <ListingItem listing={listing.data} id={listing.id} key={listing.id} />
                     ))
                 }
             </ul>
