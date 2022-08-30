58c2a90c3d8a4d8b4ae58dd0d425f05b6da5fc29
phillip khoza <phillipantonk@gmail.com>
2022-08-30 22:59:09 +0200

    create listing button

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,8 +2,11 @@ import {useState } from 'react';
 import {getAuth, updateProfile} from 'firebase/auth';
 import {doc, updateDoc} from 'firebase/firestore';
 import {db} from '../firebase.config';
-import { useNavigate} from 'react-router-dom';
+import { Link, useNavigate} from 'react-router-dom';
 import {toast} from 'react-toastify';
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg';
+import homeIcon from '../assets/svg/homeIcon.svg';
+
 
 function Profile() {
     const auth = getAuth();
@@ -64,6 +67,11 @@ function Profile() {
                     <input type="email" id='email' className={!changeDetails ? 'profileEmail' : 'profileEmailActive'} disabled={!changeDetails} value={email} onChange={onChange}/>
                 </form>
             </div>
+            <Link to='/create-listing' className='createListing'>
+                <img src={homeIcon} alt="home" />
+                <p>Sell or rent your home</p>
+                <img src={arrowRight} alt="arrow right" />
+            </Link>
         </main>
     </div>
 }
