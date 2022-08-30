9af8e2babe6c7e70d5911639a1cf5a44658356c0
=Gideon Ogongo <=ogjr80@gmail.com>
2022-08-30 17:37:51 +0200

    add private route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/Navbar';
+import PrivateRoute from './components/PrivateRoute';
 import Explore from './pages/Explore';
 import Offers from './pages/Offers';
 import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
+
 
 function App() {
   return (
@@ -16,7 +18,9 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore/>}/>
           <Route path='/offers' element={<Offers/>}/>
-          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/profile' element={<PrivateRoute/>}>
+            <Route path='/profile' element={<Profile/>}/>
+          </Route>
           <Route path='/sign-in' element={<SignIn/>}/>
           <Route path='/sign-up' element={<SignUp/>}/>
           <Route path='/forgot-password' element={<ForgotPassword/>}/>
