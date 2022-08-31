5d0aedac19c84dee088e8719e4318822e97a3856
phillip khoza <phillipantonk@gmail.com>
2022-08-31 16:27:43 +0200

    contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -25,10 +25,38 @@ function Contact() {
         }
 
         getLandlord();
-    },[params.landlordId])
+    },[params.landlordId]);
+
+    const onChange = (e) =>{
+        setMessage(e.target.value);
+
+    }
 
     return ( 
-        <div>Contact</div>
+        <div className="pageContainer">
+            <header>
+                <p className="pageHeader">Contact Landlord</p>
+            </header>
+            {landlord !== null &&(
+                <main>
+                    <div className="contactLandlord">
+                        <p className="landlordName">Contact: {landlord?.name}</p>
+
+                    </div>
+                    <form className="messageForm">
+                        <div className="messageDiv">
+                            <label htmlFor="message" className="messageLabel">Message</label>
+                            <textarea name="message" id="message"className="textarea" value={message} onChange={onChange}></textarea>
+                        <div>
+                        <a href={`mailto:${landlord.email}?Subject=${searchParams.get('listName')}&body=${message}`} className="anchorButton">
+                            <button type="button" className="primaryButton">Send Message</button>
+                        </a>
+                        </div>
+                        </div>
+                    </form>
+                </main>
+            )}
+        </div>
      );
 }
 
