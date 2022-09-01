44d786b10a4724256d0e687bfa24dcc496bf9465
phillip khoza <phillipantonk@gmail.com>
2022-09-01 10:44:24 +0200

    load more listing

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -10,6 +10,8 @@ function Offers() {
 
     const [ listings, setListings] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [lastFetchedListing, setLastFetchedListing] = useState(null);
+
     // const params = useParams();
 
     useEffect(()=>{
@@ -20,6 +22,10 @@ function Offers() {
                 const querylistings = query(listingsRef, where('offer', '==', true), orderBy('timestamp', 'desc',limit(10)));
 
                 const qurySnap = await getDocs(querylistings);
+                
+                const lastVisible = qurySnap.docs[qurySnap.docs.length-1];
+
+                setLastFetchedListing(lastVisible);
 
                 const listings = [];
 
@@ -34,7 +40,31 @@ function Offers() {
         }
         fetchListings()
     },[])
+    // pagination/ load more
+    const onFetchMoreListings = async () =>{
+        try {
+            const listingsRef = collection(db, 'listings');
+            // console.log(params.categoryName);
+            const querylistings = query(listingsRef,  where('offer', '==', true), orderBy('timestamp', 'desc',startAfter(lastFetchedListing),limit(10)));
+            
+            
+            const qurySnap = await getDocs(querylistings);
+            
+            const lastVisible = qurySnap.docs[qurySnap.docs.length-1];
 
+            setLastFetchedListing(lastVisible);
+            
+            const listings = [];
+
+            qurySnap.forEach((doc)=>{
+                return listings.push({id: doc.id, data:doc.data()})
+            });
+            setListings((prevState)=> [...prevState,...listings]);
+            setLoading(false);
+        } catch (error) {
+            toast.error('could not fetch listings')
+        }
+    }
     return <div className="category">
         <header>
         <p className="pageHeader">Offers</p>
@@ -50,6 +80,11 @@ function Offers() {
                 }
             </ul>
         </main>
+        <br />
+        <br />
+        {lastFetchedListing && (
+            <p className="loadMore" onClick={onFetchMoreListings}>Load More</p>
+        )}
         </>
         : <p>There are no current offers</p>}
         </div>;
